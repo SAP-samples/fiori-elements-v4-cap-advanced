3b341bdf7d972755bca721e8dc1f9536d5b6bb2c
Tatiana Medvedeva <tatiana.medvedeva@sap.com>
2023-01-26 12:47:53 +0000

    generate customer app and adjust list report

---
+++ app/customer/webapp/test/integration/FirstJourney.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+    "sap/ui/test/opaQunit"
+], function (opaTest) {
+    "use strict";
+
+    var Journey = {
+        run: function() {
+            QUnit.module("First journey");
+
+            opaTest("Start application", function (Given, When, Then) {
+                Given.iStartMyApp();
+
+                Then.onThePassengerList.iSeeThisPage();
+
+            });
+
+
+            opaTest("Navigate to ObjectPage", function (Given, When, Then) {
+                // Note: this test will fail if the ListReport page doesn't show any data
+                When.onThePassengerList.onFilterBar().iExecuteSearch();
+                Then.onThePassengerList.onTable().iCheckRows();
+
+                When.onThePassengerList.onTable().iPressRow(0);
+                Then.onThePassengerObjectPage.iSeeThisPage();
+
+            });
+
+            opaTest("Teardown", function (Given, When, Then) { 
+                // Cleanup
+                Given.iTearDownMyApp();
+            });
+        }
+    }
+
+    return Journey;
+});
