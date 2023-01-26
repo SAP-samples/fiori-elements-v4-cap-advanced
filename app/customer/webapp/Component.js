107baa2c93c41c8a6782759fe02097658c055b66
Tatiana Medvedeva <tatiana.medvedeva@sap.com>
2023-01-26 12:47:53 +0000

    configure object page

---
+++ app/customer/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("sap.fe.cap.customer.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
