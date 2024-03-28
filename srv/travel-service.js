e4b498c3a23d99d69bdbfd282d315dd3cca980c7
Hitesh Parmar <hitesh.parmar@sap.com>
2024-03-28 17:55:08 +0100

    create action with a mandatory parameter

--- srv/travel-service.js
+++ srv/travel-service.js
@@ -145,6 +145,25 @@ init() {
   this.on ('acceptTravel', req => UPDATE (req.subject) .with ({TravelStatus_code:'A'}))
   this.on ('rejectTravel', req => UPDATE (req.subject) .with ({TravelStatus_code:'X'}))
 
+  this.on ('deductDiscount', async req => {
+    let discount = req.data.percent / 100
+    let succeeded = await UPDATE (req.subject)
+      .where `TravelStatus_code != 'A'`
+      .and `BookingFee is not null`
+      .with (`
+        TotalPrice = round (TotalPrice - BookingFee * ${discount}, 3),
+        BookingFee = round (BookingFee - BookingFee * ${discount}, 3)
+      `)
+    if (!succeeded) { //> let's find out why...
+      let travel = await SELECT.one `TravelID as ID, TravelStatus_code as status, BookingFee` .from (req.subject)
+      if (!travel) throw req.reject (404, `Travel "${travel.ID}" does not exist; may have been deleted meanwhile.`)
+      if (travel.status === 'A') req.reject (400, `Travel "${travel.ID}" has been approved already.`)
+      if (travel.BookingFee == null) throw req.reject (404, `No discount possible, as travel "${travel.ID}" does not yet have a booking fee added.`)
+    } else {
+      return this.read(req.subject)
+    }
+  })
+  
   // Add base class's handlers. Handlers registered above go first.
   return super.init()
 
