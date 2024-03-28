37eeb0b2eb3d8c01d291992bee077cd252ea6d2a
Hitesh Parmar <hitesh.parmar@sap.com>
2024-03-28 17:55:08 +0100

    update to cds 8

--- srv/travel-service.js
+++ srv/travel-service.js
@@ -23,7 +23,7 @@ init() {
   /**
    * Fill in defaults for new Bookings when editing Travels.
    */
-  this.before ('NEW', 'Booking', async (req) => {
+  this.before ('CREATE', 'Booking.drafts', async (req) => {
     const { to_Travel_TravelUUID } = req.data
     const { status } = await SELECT `TravelStatus_code as status` .from (Travel.drafts, to_Travel_TravelUUID)
     if (status === 'X') throw req.reject (400, 'Cannot add new bookings to rejected travels.')
@@ -37,7 +37,7 @@ init() {
   /**
    * Fill in defaults for new BookingSupplements when editing Travels.
    */
-  this.before ('NEW', 'BookingSupplement', async (req) => {
+  this.before ('CREATE', 'BookingSupplement.drafts', async (req) => {
     const { to_Booking_BookingUUID } = req.data
     const { maxID } = await SELECT.one `max(BookingSupplementID) as maxID` .from (BookingSupplement.drafts) .where ({to_Booking_BookingUUID})
     req.data.BookingSupplementID = maxID + 1
@@ -47,8 +47,8 @@ init() {
   /**
    * Changing Booking Fees is only allowed for not yet accapted Travels.
    */
-  this.before ('PATCH', 'Travel', async (req) => { if ('BookingFee' in req.data) {
-    const { status } = await SELECT `TravelStatus_code as status` .from (req._target)
+  this.before ('UPDATE', 'Travel.drafts', async (req) => { if ('BookingFee' in req.data) {
+    const { status } = await SELECT.one `TravelStatus_code as status` .from (req.subject)
     if (status === 'A') req.reject(400, 'Booking fee can not be updated for accepted travels.', 'BookingFee')
   }})
 
@@ -56,7 +56,7 @@ init() {
   /**
    * Update the Travel's TotalPrice when its BookingFee is modified.
    */
-  this.after ('PATCH', 'Travel', (_,req) => { if ('BookingFee' in req.data) {
+  this.after ('UPDATE', 'Travel.drafts', (_,req) => { if ('BookingFee' in req.data) {
     return this._update_totals4 (req.data.TravelUUID)
   }})
 
@@ -64,9 +64,9 @@ init() {
   /**
    * Update the Travel's TotalPrice when a Booking's FlightPrice is modified.
    */
-  this.after ('PATCH', 'Booking', async (_,req) => { if ('FlightPrice' in req.data) {
+  this.after ('UPDATE', 'Booking.drafts', async (_,req) => { if ('FlightPrice' in req.data) {
     // We need to fetch the Travel's UUID for the given Booking target
-    const { travel } = await SELECT.one `to_Travel_TravelUUID as travel` .from (req._target)
+    const { travel } = await SELECT.one `to_Travel_TravelUUID as travel` .from (req.subject)
     return this._update_totals4 (travel)
   }})
 
@@ -74,24 +74,24 @@ init() {
   /**
    * Update the Travel's TotalPrice when a Supplement's Price is modified.
    */
-  this.after ('PATCH', 'BookingSupplement', async (_,req) => { if ('Price' in req.data) {
+  this.after ('UPDATE', 'BookingSupplement.drafts', async (_,req) => { if ('Price' in req.data) {
     // We need to fetch the Travel's UUID for the given Supplement target
+    const { booking } = await SELECT.one `to_Booking_BookingUUID as booking`
+      .from (BookingSupplement.drafts).where({BookSupplUUID:req.data.BookSupplUUID})
     const { travel } = await SELECT.one `to_Travel_TravelUUID as travel` .from (Booking.drafts)
-      .where `BookingUUID = ${ SELECT.one `to_Booking_BookingUUID` .from (BookingSupplement.drafts).where({BookSupplUUID:req.data.BookSupplUUID}) }`
-      // .where `BookingUUID = ${ SELECT.one `to_Booking_BookingUUID` .from (req._target) }`
-      //> REVISIT: req._target not supported for subselects -> see tests
+      .where `BookingUUID = ${booking} `
     return this._update_totals4 (travel)
   }})
 
   /**
    * Update the Travel's TotalPrice when a Booking Supplement is deleted.
    */
-  this.on('CANCEL', BookingSupplement, async (req, next) => {
+  this.on('DELETE', BookingSupplement.drafts, async (req, next) => {
     // Find out which travel is affected before the delete
-    const { DraftAdministrativeData_DraftUUID, BookSupplUUID } = req.data
+    const { BookSupplUUID } = req.data
     const { to_Travel_TravelUUID } = await SELECT.one
       .from(BookingSupplement.drafts, ['to_Travel_TravelUUID'])
-      .where({ DraftAdministrativeData_DraftUUID, BookSupplUUID })
+      .where({ BookSupplUUID })
     // Delete handled by generic handlers
     const res = await next()
     // After the delete, update the totals
@@ -102,12 +102,12 @@ init() {
   /**
    * Update the Travel's TotalPrice when a Booking is deleted.
    */
-  this.on('CANCEL', Booking, async (req, next) => {
+  this.on('DELETE', Booking.drafts, async (req, next) => {
     // Find out which travel is affected before the delete
-    const { DraftAdministrativeData_DraftUUID, BookingUUID } = req.data
+    const { BookingUUID } = req.data
     const { to_Travel_TravelUUID } = await SELECT.one
       .from(Booking.drafts, ['to_Travel_TravelUUID'])
-      .where({ DraftAdministrativeData_DraftUUID, BookingUUID })
+      .where({ BookingUUID })
     // Delete handled by generic handlers
     const res = await next()
     // After the delete, update the totals
@@ -120,10 +120,10 @@ init() {
    * Helper to re-calculate a Travel's TotalPrice from BookingFees, FlightPrices and Supplement Prices.
    */
   this._update_totals4 = function (travel) {
-    return UPDATE (Travel.drafts, travel) .with ({ TotalPrice: CXL `coalesce (BookingFee, 0) + ${
-      SELECT `coalesce (sum (FlightPrice + ${
-        SELECT `coalesce (sum (Price),0)` .from (BookingSupplement.drafts) .where `to_Booking_BookingUUID = BookingUUID`
-      }),0)` .from (Booking.drafts) .where `to_Travel_TravelUUID = TravelUUID`
+    return UPDATE (Travel.drafts, travel) .alias('T') .with ({ TotalPrice: CXL `coalesce (T.BookingFee, 0) + ${
+      SELECT `coalesce (sum (B.FlightPrice + ${
+        SELECT `coalesce (sum (BS.Price),0)` .from (BookingSupplement.drafts) .alias('BS') .where `BS.to_Booking_BookingUUID = B.BookingUUID`
+      }),0)` .from (Booking.drafts) .alias('B') .where `B.to_Travel_TravelUUID = T.TravelUUID`
     }` })
   }
 
@@ -142,8 +142,8 @@ init() {
   // Action Implementations...
   //
 
-  this.on ('acceptTravel', req => UPDATE (req._target) .with ({TravelStatus_code:'A'}))
-  this.on ('rejectTravel', req => UPDATE (req._target) .with ({TravelStatus_code:'X'}))
+  this.on ('acceptTravel', req => UPDATE (req.subject) .with ({TravelStatus_code:'A'}))
+  this.on ('rejectTravel', req => UPDATE (req.subject) .with ({TravelStatus_code:'X'}))
 
   // Add base class's handlers. Handlers registered above go first.
   return super.init()
